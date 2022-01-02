0989922625bd701ef84bd9ce4cada4c5c4c9c85d
Canachii <42132716+Canachii@users.noreply.github.com>
2022-01-03 00:51:42 +0900

    Add files via upload

---
+++ addons/TTT2 Korean/lua/terrortown/lang/ko/roundinfo.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("ko")
+
+L["ttt_rs_preText"] = "0%이번 라운드에는 %1%트레이터%0%가 {traits}명, %2%이노센트%0%가 {innos}명, 그리고 %3%관전자%0%가 {specs}명 있습니다."
+L["ttt_rs_postText"] = "이번 라운드의 플레이어 역할:"
+L["ttt_rs_killText"] = "0%당신은 %1%{killer}%0%에게 살해당했습니다. 역할:%2%{role}%0%. (무기: {killtype})"
+L["ttt_rs_suicideText"] = "0%자살하고 말았습니다... (사인: {killtype})"
+L["ttt_rs_worldKillText"] = "0%당신은 세계에 의해 사망했습니다."
+
+L["ttt_rs_killtype_unknown"] = "불명"
+L["ttt_rs_killtype_propkill"] = "프롭킬"
+L["ttt_rs_killtype_falldamage"] = "추락사"
+L["ttt_rs_killtype_firedamage"] = "소사"
+L["ttt_rs_killtype_explosion"] = "폭사"
+L["ttt_rs_killtype_drowned"] = "익사"
+
+L["ttt_rs_you_were_killed"] ="당신은 사망했습니다. 살해자는"
+L["ttt_rs_killer_yourself"] ="자신"
+L["ttt_rs_killer_world"] = "세계"
---
+++ addons/TTT2 Korean/lua/terrortown/lang/ko/survivalist.lua
@@ -0,0 +1,12 @@
+local L = LANG.GetLanguageTableReference("ko")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SURVIVALIST.name] = "서바이벌리스트"
+L["info_popup_" .. SURVIVALIST.name] = [[당신은 서바이벌리스트입니다!
+가능하면 살아남고 여러분의 동료를 보호하도록 노력하세요!]]
+L["body_found_" .. SURVIVALIST.abbr] = "그는 서바이벌리스트였습니다."
+L["search_role_" .. SURVIVALIST.abbr] = "이 사람은 서바이벌리스트였습니다!"
+L["target_" .. SURVIVALIST.name] = "서바이벌리스트"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[서바이벌리스트는 그들 자신의 상점에 접근할 수 있기 때문에 더 나은 이노센트입니다.
+
+이노센트들을 보호하려고 노력하세요!]]
