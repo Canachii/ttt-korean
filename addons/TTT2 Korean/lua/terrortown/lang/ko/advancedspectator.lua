bda15d583ef46c1754e7f3de16b4650169342f07
Canachii <42132716+Canachii@users.noreply.github.com>
2022-01-08 02:03:48 +0900

    Add files via upload

---
+++ addons/TTT2 Korean/lua/terrortown/lang/ko/advancedspectator.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("ko")
+
+L["ttt2_aspectator_wallhack"] = "어드민만 벽을 투시하는 기능 전환"
+L["ttt2_aspectator_started_wallack"] = "어드민만 벽을 투시하는 기능을 킵니다."
+L["ttt2_aspectator_stopped_wallack"] = "어드민만 벽을 투시하는 기능을 끕니다."
+
+L["submenu_server_addons_advancedspectator_title"] = "향상된 관전자"
+L["header_addons_advancedspectator_hud"] = "향상된 관전자 HUD 설정"
+L["header_addons_advancedspectator_wallhack"] = "향상된 관전자 벽 투시 설정"
+
+L["label_aspectator_display_role"] = "HUD로 플레이어의 역할 보이기"
+L["label_aspectator_admin_only"] = "HUD를 어드민에게만 보이기"
+L["label_aspectator_enable_wallhack"] = "관전하는 동안 벽 투시를 활성화"
+L["label_aspectator_display_wallhack_role"] = "관전자에게 역할 보이기"
+L["label_aspectator_admin_wallhack"] = "역할 디버그를 위한 어드민 벽 투시 활성화"
+
+L["help_aspectator_admin_wallhack"] = "어드민 벽 투시 기능을 활성화해도 자동으로 시작되지 않습니다. 키 설정 메뉴에서 키를 설정해야 합니다. 키를 사용하여 벽 투시를 전환합니다."
